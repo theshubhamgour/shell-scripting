2614d94c43f671ed4efac077b9010a90a6edfeac
Shubham Gour <iamtheshubhamgour@gmail.com>
2023-10-24 16:55:13 +0530

    Added arguments

---
+++ fifth.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "I will be a DevOps engineer one day"
+
+#Argument - you need to run the file as ./fifth.sh {name} which you want to display
+echo "Hi What is your name?"
+
+sleep 2
+
+echo "Hello my name is $1"
